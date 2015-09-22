f86ce16a85c79a8b9a2fce6ff1aa726036f861a4
MillsMultimedia <jeff@millsmultimedia.net>
2015-09-22 18:51:33 -0500

    final version with cheesy space design

--- README.md
+++ README.md
@@ -7,10 +7,11 @@
 Random xkcd style password generator with words from user choice of Star Wars or Star Trek
 
 ## Demo
-n/a
+http://www.screencast.com/t/I2PvbVNavN6s
 
 ## Details for teaching team
-n/a
+I'm running OS 10.10.5 so jing was giving me trouble. I used another screen capture program that I already had and uploaded to screencast.com.
+Hopefully, upgrading to El Capitan in a few days will fix this.
 
 ## Outside code
 * Bootstrap: https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js
--- index.php
+++ index.php
@@ -12,37 +12,43 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <?php require 'php/pw.php' ?>
-    <title>Bootstrap</title>
+    <title>Project 2: Password Generator</title>
 </head>
 <body>
 
 <div class="container">
-<h1 class="text-center">Nerdy Password Generator</h1>
-<p class="text-center">Fill out the form to generate a secure password using your favorite movies</p>
+	<div class="col-md-8 col-md-offset-2" id="wrapper">
+		<h1 class="text-center">NERDY PASSWORD GENERATOR</h1>
+		<h3 class="text-center" id="instructions">Fill out the form to generate a secure password<br>using your favorite movies</h3>
 
-<form method='POST' action='index.php'>
-	<div class="form-group text-center">
-		<h4>Pick your favorite</h4>
-		<div class="radio">
-			<label class="radio-inline"><input type="radio" name="fav" value="wars">Star Wars</label>
-			<label class="radio-inline"><input type="radio" name="fav" value="trek">Star Trek</label>
-		</div>
+		<form method='POST' action='index.php'>
+			<div class="form-group text-center">
+				<h4>Pick your favorite</h4>
+				<div class="radio">
+					<label class="radio-inline"><input type="radio" name="fav" value="wars">Star Wars</label>
+					<label class="radio-inline"><input type="radio" name="fav" value="trek">Star Trek</label>
+				</div>
+			</div>
 
-		<label>Number of Words (max 5):</label> <input type="text" name="wordCount" style="width: 40px;">
+			<div class="form-group text-center">
+				<label>Number of Words (max 5):</label> <input type="text" name="wordCount" style="width: 40px;color:#000;">
+			</div>
 
-		<h4>Does your password require any of the following (check all that apply):</h4>
-		<div class="radio">
-		  <label><input type="radio" name="caps" value="caps">Capital Letters</label><br>
-		  <label><input type="radio" name="chars" value="chars">Special Characters</label>
-		</div>
-		<input type="submit" value="Nerd Me">
-</div>
-</form>
+			<div class="form-group text-center">
+				<h4>Does your password require any of the following (check all that apply):</h4>
+				<div class="radio">
+				  <label><input type="radio" name="caps" value="caps">Capital Letters &nbsp; </label>
+				  <label><input type="radio" name="chars" value="chars">Special Characters</label>
+				</div>
+				<input type="submit" value="Nerd Me" id="submit">
+			</div>
+		</form>
 
-<h3 class="text-center">
-	<?php echo $output; ?>
-</h3>
-
+		<h3 class="text-center col-md-8 col-md-offset-2 col-xs-12" id="password">
+			<?php echo $output; ?>
+		</h3>
+		<div class="clearfix"></div>
+	</div>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
